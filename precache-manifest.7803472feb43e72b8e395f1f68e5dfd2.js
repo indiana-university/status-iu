5a1f6e6de4531a787de57e8980bb7cc54aa36490
Josh LaMar <jolamar@iu.edu>
2018-10-02 14:55:55 -0600

    Updates

---
+++ precache-manifest.7803472feb43e72b8e395f1f68e5dfd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66cdef7850f05b1cbbfa",
+    "url": "/status-iu/static/js/runtime~main.66cdef78.js"
+  },
+  {
+    "revision": "0283a48dd8d65771f00a",
+    "url": "/status-iu/static/js/main.0283a48d.chunk.js"
+  },
+  {
+    "revision": "aafed53ac62f79681015",
+    "url": "/status-iu/static/js/1.aafed53a.chunk.js"
+  },
+  {
+    "revision": "aafed53ac62f79681015",
+    "url": "/status-iu/static/css/1.5d23443c.chunk.css"
+  },
+  {
+    "revision": "d50dd2d25074bc15a4351391f36fbd64",
+    "url": "/status-iu/index.html"
+  }
+];
