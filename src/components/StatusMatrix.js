ebdf34397983c9801ee9689b825be35b4f9ced51
Josh LaMar <jolamar@iu.edu>
2018-10-04 13:37:23 -0600

    list services under service group

--- src/components/StatusMatrix.js
+++ src/components/StatusMatrix.js
@@ -3,28 +3,7 @@ import 'rivet-uits/css/rivet.css'
 import { Table } from 'rivet-react'
 import { notices, serviceGroups, services } from '../status-api'
 import './StatusMatrix.css'
-
-const checkmark = <svg alt="Service is online and healthy." xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-  <path fill="currentColor" d="M6.72,13l-.48-.36-3-3A1,1,0,0,1,4.71,8.29l2.11,2.12,4.33-6.94a1,1,0,0,1,1.7,1.06L7.64,12.87Z"/>
-</svg>
-
-const rss = <svg alt="RSS feed" xmlns="http://www.w3.org/2000/svg" viewBox="0 -256 1792 1792" width="16" height="16">
-  <g transform="matrix(1,0,0,-1,212.61017,1346.1695)">
-    <path
-    d="M 384,192 Q 384,112 328,56 272,0 192,0 112,0 56,56 0,112 0,192 q 0,80 56,136 56,56 136,56 80,0 136,-56 56,-56 56,-136 z M 896,69 Q 898,41 879,21 861,0 832,0 H 697 Q 672,0 654,16.5 636,33 634,58 612,287 449.5,449.5 287,612 58,634 33,636 16.5,654 0,672 0,697 v 135 q 0,29 21,47 17,17 43,17 h 5 Q 229,883 375,815.5 521,748 634,634 748,521 815.5,375 883,229 896,69 z m 512,-2 Q 1410,40 1390,20 1372,0 1344,0 H 1201 Q 1175,0 1156.5,17.5 1138,35 1137,60 1125,275 1036,468.5 947,662 804.5,804.5 662,947 468.5,1036 275,1125 60,1138 35,1139 17.5,1157.5 0,1176 0,1201 v 143 q 0,28 20,46 18,18 44,18 h 3 Q 329,1395 568.5,1288 808,1181 994,994 1181,808 1288,568.5 1395,329 1408,67 z"
-    id="path2993"
-    fill="currentColor" />
-  </g>
-</svg>
-
-const chevronUp = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-  <path fill="currentColor" d="M14,12a1,1,0,0,1-.76-.35L8,5.54,2.76,11.65a1,1,0,1,1-1.52-1.3L6.48,4.23a2.06,2.06,0,0,1,3,0l5.24,6.11A1,1,0,0,1,14,12Z"/>
-</svg>
-
-const chevronDown = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-  <path fill="currentColor" d="M8,12.46a2,2,0,0,1-1.52-.7L1.24,5.65a1,1,0,1,1,1.52-1.3L8,10.46l5.24-6.11a1,1,0,0,1,1.52,1.3L9.52,11.76A2,2,0,0,1,8,12.46Z"/>
-</svg>
-
+import {checkmark, chevronUp, rss} from '../icons'
 
 
 export class StatusMatrix extends Component {
@@ -38,6 +17,11 @@ export class StatusMatrix extends Component {
       serviceGroups: [],
       services: []
     };
+    this.isPartOfGroup = this.isPartOfGroup.bind(this)
+  }
+
+  isPartOfGroup(service, group) {
+    return service.serviceGroup.id === group.id
   }
 
   componentDidMount() {
@@ -82,20 +66,20 @@ export class StatusMatrix extends Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.serviceGroups.map((category) =>
-            <React.Fragment key={category.id}>
+          {this.state.serviceGroups.map((group) =>
+            <React.Fragment key={group.id}>
               <tr className="status-matrix__header-row">
                 <td className="status-matrix__category">
                   <button className="rvt-button--plain status-matrix__category-toggle js-toggle__trigger">
                     {chevronUp}
-                    <span className="rvt-m-left-xs">{category.name}</span>
+                    <span className="rvt-m-left-xs">{group.name}</span>
                   </button>
                   <a href="https://status.uits.iu.edu/Rss?services=TODO,TODO"
                      target="_blank"
                      rel="noopener noreferrer"
                      className="status-matrix__category-rss">
                     {rss}
-                    <span className="rvt-sr-only">RSS feed for {category.name}</span>
+                    <span className="rvt-sr-only">RSS feed for {group.name}</span>
                   </a>
                 </td>
                 <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
@@ -108,23 +92,29 @@ export class StatusMatrix extends Component {
                 <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
                 <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
               </tr>
-              <tr className="status-matrix__sub-row js-toggle__target">
-                <td className="status-matrix__service">
-                  <a className="status-matrix__service-title" href="https://status.uits.iu.edu/service/TODO">Service under {category.name}</a>
-                  <a href="https://status.uits.iu.edu/Rss?services=TODO" target="_blank" rel="noopener noreferrer" className="status-matrix__service-rss">
-                    {rss}
-                  </a>
-                </td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-                <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
-              </tr>
+              {this.state.services.map((service) =>
+                <React.Fragment key={service.id}>
+                  { this.isPartOfGroup(service, group) &&
+                    <tr className="status-matrix__sub-row js-toggle__target">
+                      <td className="status-matrix__service">
+                        <a className="status-matrix__service-title" href="https://status.uits.iu.edu/service/TODO">{service.name}</a>
+                        <a href="https://status.uits.iu.edu/Rss?services=TODO" target="_blank" rel="noopener noreferrer" className="status-matrix__service-rss">
+                          {rss}
+                        </a>
+                      </td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                      <td className="status-icon status-icon--good rvt-color-green">{checkmark}</td>
+                    </tr>
+                  }
+                </React.Fragment>
+              )}
             </React.Fragment>
           )}
         </tbody>
