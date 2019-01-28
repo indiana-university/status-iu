74075a552c47bee22d717880a5675eaa65bcb2a7
Austin Lord <ahlord@iu.edu>
2019-01-28 10:33:06 -0500

    start from scratch

--- README.md
+++
@@ -1,2 +0,0 @@
-# Status.IU
-Status.IU Frontend uses Create React App + Rivet React + Rivet 1.1.0
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export * from './Calendar';
-export * from './Home';
-export * from './Maintenance';
-export * from './Notice';
-export * from './Notices';
