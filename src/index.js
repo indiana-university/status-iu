7d03bdbd4072e0741fa5d3ce9f81eb7baf8d5f4f
Rachel O'Connor <racmocon@indiana.edu>
2019-01-30 16:55:03 -0500

    Added rivet.min.css.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./scss/index.scss";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
+import 'rivet-uits/css/rivet.min.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
