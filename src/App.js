50762f170dd81e4ee0b61428b2454801dd4735d6
Rachel O'Connor <racmocon@indiana.edu>
2019-01-31 14:19:28 -0500

    Created area for content.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { AppHeader, Navigation } from './components';
-import { Footer } from "rivet-react";
+import { Container, Footer } from "rivet-react";
 
 class App extends Component {
   render() {
@@ -8,6 +8,9 @@ class App extends Component {
       <div className="App">
         <AppHeader />
         <Navigation />
+        <main id="main-content">
+          <Container width="junior" center></Container>
+        </main>
         <Footer>
           <a href="/privacy">Privacy Policy</a>
           <a href="https://accessibility.iu.edu/" target="_blank" rel="noopener noreferrer">Accessibility help</a>
