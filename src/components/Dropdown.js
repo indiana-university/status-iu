26f351fc37d9618b076e5be3aef48c2c373b7007
Rachel O'Connor <racmocon@indiana.edu>
2019-02-04 15:25:03 -0500

    Changes header dropdown to accept icon variable.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,18 @@
 import React from "react";
-import {CaratDown} from "../icons"
+import { CaratDown, IconInformation } from "../icons"
 
-export const Dropdown = ({menu, alerts}) => (
+export const Dropdown = ({menu, alerts, icon}) => (
   <div className="rvt-dropdown">
-    <button className="rvt-button rvt-button--plain rvt-p-all-remove siu-info-toggle" data-dropdown-toggle="dropdown-navigation" aria-haspopup="true" aria-expanded="false">
+    { icon === true ? (
+      <button className="rvt-button rvt-button--plain siu-info-toggle" data-dropdown-toggle="dropdown-navigation" aria-haspopup="true" aria-expanded="false">
+        { IconInformation }<span className="rvt-m-left-xs">{menu}</span>
+      </button>
+    ) : (
+      <button className="rvt-button rvt-button--plain siu-info-toggle" data-dropdown-toggle="dropdown-navigation" aria-haspopup="true" aria-expanded="false">
         <span className="rvt-m-left-xs">{menu}</span>
         <CaratDown className="rvt-m-left-xs" />
-    </button>
+      </button>
+    )}
     <div className="rvt-box rvt-box--card rvt-dropdown__menu rvt-dropdown__menu--right rvt-dropdown__menu--large" id="dropdown-navigation" aria-hidden="true" role="menu">
         <a key="1" href="./fake">{alerts}</a>
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { List } from "rivet-react";
 import { Dropdown } from './index';
+import { IconCalendar, IconMaintenance } from '../icons';
 
 export const Navigation = () => (
-  <div className="siu-wrapper">
-    <nav className="siu-main-nav" aria-label="Site navigation">
-      <List className="siu-main-nav__list">
-        <li className="siu-main-nav__item">
-          <a className="siu-plain-link" href="./maintenance">Maintenance</a>
-        </li>
-        <li className="siu-main-nav__item">
-          <a className="siu-plain-link" href="./calendar">Calendar</a>
-        </li>
-        <li className="siu-main-nav__item"><Dropdown menu="Information" alerts="Turnitin Similarity Report score discrepancy in Canvas" /></li>
-      </List>
-    </nav>
+  <div className="siu-wrapper siu-wrapper--crimson">
+    <div className="rvt-container rvt-container--junior rvt-container--center">
+      <nav className="siu-main-nav" aria-label="Site navigation">
+        <List className="siu-main-nav__list">
+          <li className="siu-main-nav__item">
+            <a className="rvt-display-flex rvt-vertical-center" href="./maintenance">{ IconMaintenance }<span className="rvt-m-left-xs">Maintenance</span></a>
+          </li>
+          <li className="siu-main-nav__item">
+            <a className="rvt-display-flex rvt-vertical-center" href="./calendar">{IconCalendar}<span className="rvt-m-left-xs">Calendar</span></a>
+          </li>
+          <li className="rvt-display-flex rvt-vertical-center" style={{ height: '24px' }}><Dropdown menu="Information" alerts="Turnitin Similarity Report score discrepancy in Canvas" icon={ true } /></li>
+        </List>
+      </nav>
+    </div>
   </div>
 );
